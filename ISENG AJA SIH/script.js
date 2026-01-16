0137e585a2753f9e9e9853d7fffc62ba56bc3e16
Algh-px <yusufalghifaria@gmail.com>
2026-01-16 19:00:31 +0700

    Upload pertama

---
+++ ISENG AJA SIH/script.js
@@ -0,0 +1,25 @@
+const message = `HEHEHEHEHE, SEMANGATTTT, HEHEHEHEHEHE
+SEMANGAT YAAA, BEKASI LAGI DINGIN
+JAGA KESEHATANN SEMUAAAAAAAA`;
+
+function showLetter() {
+  document.getElementById("introText").style.opacity = 0;
+  document.querySelector(".btn").style.display = "none";
+
+  setTimeout(() => {
+    const letterBox = document.getElementById("letterBox");
+    const typedText = document.getElementById("typedText");
+    letterBox.style.display = "block";
+    let i = 0;
+
+    function typeWriter() {
+      if (i < message.length) {
+        typedText.innerHTML += message.charAt(i);
+        i++;
+        setTimeout(typeWriter, 30);
+      }
+    }
+
+    typeWriter();
+  }, 600);
+}
